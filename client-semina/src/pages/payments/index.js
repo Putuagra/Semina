6277bb8366f090190d61556451c7fb3885d16690
putuagra <iputuagra@gmail.com>
2024-08-23 10:29:29 +0700

    feat: add payments page

---
+++ client-semina/src/pages/payments/index.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import { Container, Tab } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import SBreadCrumb from "../../components/Breadcrumb";
+import Button from "../../components/Button";
+import Table from "../../components/TableWithAction";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPayments } from "../../redux/payments/actions";
+import SAlert from "../../components/Alert";
+import Swal from "sweetalert2";
+import { deleteData } from "../../utils/fetch";
+import { setNotification } from "../../redux/notifikasi/actions";
+import { accessPayments } from "../../const/access";
+
+export default function PagePayments() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const notification = useSelector((state) => state.notification);
+  const payments = useSelector((state) => state.payments);
+
+  const [access, setAccess] = useState({
+    tambah: false,
+    hapus: false,
+    edit: false,
+  });
+
+  const checkAccess = () => {
+    let { role } = localStorage.getItem("auth")
+      ? JSON.parse(localStorage.getItem("auth"))
+      : {};
+    const access = { tambah: false, hapus: false, edit: false };
+    Object.keys(accessPayments).forEach(function (key, index) {
+      if (accessPayments[key].indexOf(role) >= 0) {
+        access[key] = true;
+      }
+    });
+    setAccess(access);
+  };
+
+  useEffect(() => {
+    checkAccess();
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchPayments());
+  }, [dispatch]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Apa kamu yakin?",
+      text: "Anda tidak akan dapat mengembalikan ini!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Iya, Hapus",
+      cancelButtonText: "Batal",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await deleteData(`/cms/payments/${id}`);
+
+        dispatch(
+          setNotification(
+            true,
+            "success",
+            `berhasil hapus kategori ${res.data.data.type}`
+          )
+        );
+
+        dispatch(fetchPayments());
+      }
+    });
+  };
+  return (
+    <Container className="mt-3">
+      <SBreadCrumb textSecond={"Payments"} />
+
+      {access.tambah && (
+        <Button className={"mb-3"} action={() => navigate("/payments/create")}>
+          Tambah
+        </Button>
+      )}
+
+      {notification.status && (
+        <SAlert
+          type={notification.typeNotification}
+          message={notification.message}
+        />
+      )}
+
+      <Table
+        status={payments.status}
+        thead={["Type", "Avatar", "Aksi"]}
+        data={payments.data}
+        tbody={["type", "avatar"]}
+        editUrl={access.edit ? "/payments/edit" : null}
+        deleteAction={access.hapus ? (id) => handleDelete(id) : null}
+        withoutPagination
+      />
+    </Container>
+  );
+}
