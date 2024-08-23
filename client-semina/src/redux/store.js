35487e1c9ff877678fbbd5e9d984376c59574760
putuagra <iputuagra@gmail.com>
2024-08-23 10:26:59 +0700

    feat: update redux store

--- client-semina/src/redux/store.js
+++ client-semina/src/redux/store.js
@@ -10,6 +10,10 @@ import authReducer from "./auth/reducer";
 import categoriesReducer from "./categories/reducer";
 import notificationReducer from "./notifikasi/reducer";
 import talentsReducer from "./talents/reducer";
+import paymentsReducer from "./payments/reducer";
+import eventsReducer from "./events/reducer";
+import listsReducer from "./lists/reducer";
+import ordersReducer from "./orders/reducer";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -18,6 +22,10 @@ const rootReducers = combineReducers({
   categories: categoriesReducer,
   notification: notificationReducer,
   talents: talentsReducer,
+  payments: paymentsReducer,
+  events: eventsReducer,
+  lists: listsReducer,
+  orders: ordersReducer,
 });
 
 const store = createStore(
